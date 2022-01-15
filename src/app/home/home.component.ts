bd91301010237f150c5a75ab40aebe003ddd5d24
aman-deep1 <aman_deep@thbs.com>
2022-01-15 22:50:25 +0530

    commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,70 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  public patients:any = [];
+  public editDetails:any = {
+    editpatient : undefined,
+    editstatus : false
+  }
+
+  constructor(private http:HttpClient) { 
+
+  }
+
+  update(frm:NgForm)
+  {
+    var ob = frm.value
+    ob.patientId = this.editDetails.editpatient.patientId
+    console.log(ob)
+    this.http.put(`http://localhost:8080/patient/update`,ob).subscribe(data=>
+    {     
+      this.patients = this.patients.map((ob:any)=>ob.id==this.editDetails.editpatient.id?data:ob)
+      this.editDetails= {
+        editpatient : undefined,
+        editstatus : false
+      }
+    })
+  }
+
+  editPatient(patient:any)
+  {
+    this.editDetails = {
+      editpatient : patient,
+      editstatus : true
+    }
+  }
+
+  save(frm:NgForm){
+    console.log(frm.value)
+    this.http.post("http://localhost:8080/patient/save",frm.value).subscribe(data=>{
+        this.patients.push(data)
+    })
+  }
+
+  delPatient(pid:String){
+    this.http.delete(`http://localhost:8080/patient/del/${pid}`,{observe: 'response'}).subscribe(data=>{
+        console.log(data.status)
+        if(data.status==200){
+          this.patients = this.patients.filter((prod:any)=>prod.patientId!=pid)
+        }else{
+          alert("Not Deleted !")
+        }
+    })
+  }
+
+  ngOnInit(): void 
+  {
+    this.http.get("http://localhost:8080/patient/load").subscribe(data=>{
+      console.log(data)
+      this.patients = data
+    });
+  }
+}
